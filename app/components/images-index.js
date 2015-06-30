714a20922684d55508bbc057b028f70457e8198f
John Hampton <john_j_hampton@yahoo.com>
2015-06-30 17:06:30 -0400

    Move image data from hard-code in template over to property

--- app/components/images-index.js
+++ app/components/images-index.js
@@ -2,5 +2,16 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   tagName: 'ul',
-  classNames: ['image-index']
+  classNames: ['image-index'],
+
+  images: [
+    {
+      url: "http://lorempixel.com/400/200/sports",
+      caption: "Lorem Sports"
+    },
+    {
+      url: "http://lorempixel.com/400/200/city",
+      caption: "Lorem City"
+    }
+  ]
 });
