5275338400d6832ba9a7388677a3b73f984895aa
John Hampton <john_j_hampton@yahoo.com>
2015-06-30 17:10:02 -0400

    Move static images data from component property over to index route model

--- app/components/images-index.js
+++ app/components/images-index.js
@@ -2,16 +2,5 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   tagName: 'ul',
-  classNames: ['image-index'],
-
-  images: [
-    {
-      url: "http://lorempixel.com/400/200/sports",
-      caption: "Lorem Sports"
-    },
-    {
-      url: "http://lorempixel.com/400/200/city",
-      caption: "Lorem City"
-    }
-  ]
+  classNames: ['image-index']
 });
--- app/routes/images/index.js
+++ app/routes/images/index.js
@@ -1,4 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model: function() {
+    return [
+      {
+        url: "http://lorempixel.com/400/200/sports",
+        caption: "Lorem Sports"
+      },
+      {
+        url: "http://lorempixel.com/400/200/city",
+        caption: "Lorem City"
+      }
+    ];
+  }
 });
